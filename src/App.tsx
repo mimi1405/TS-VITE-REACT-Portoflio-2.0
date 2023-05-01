9981c6f953263f8c8327befbfa4e931bc98cb04a
Maksim Momcilovic <M.Momcilovic.inf20@stud.bbbaden.ch>
2023-05-01 11:57:16 +0200

    resolve error

--- src/App.tsx
+++ src/App.tsx
@@ -1,4 +1,5 @@
-import { Routes, Route } from "react-router-dom";
+import React from "react";
+import { Routes, Route } from "react-router";
 import Home from "./pages/Home/Home";
 import Skills from "./pages/Skills/Skills";
 import AboutMe from "./pages/AboutMe/AboutMe";
@@ -9,14 +10,16 @@ import Projects from "./pages/Projects/Projects";
 function App() {
   return (
     <>
-      <Navigation />
-      <Routes>
-        <Route path="/" element={<Home />} />
-        <Route path="/projects" element={<Projects />} />
-        <Route path="/about-me" element={<AboutMe />} />
-        <Route path="/expertise" element={<Skills />} />
-        <Route path="/contact" element={<Contact />} />
-      </Routes>
+      <div>
+        <Navigation />
+        <Routes>
+          <Route path="/" element={<Home />} />
+          <Route path="/projects" element={<Projects />} />
+          <Route path="/about-me" element={<AboutMe />} />
+          <Route path="/expertise" element={<Skills />} />
+          <Route path="/contact" element={<Contact />} />
+        </Routes>
+      </div>
     </>
   );
 }
--- src/components/Navigation/Navigation.tsx
+++ src/components/Navigation/Navigation.tsx
@@ -1,9 +1,5 @@
 import React from "react";
 import { useState, useEffect } from "react";
-import { RxHamburgerMenu } from "react-icons/rx";
-import { AiOutlineClose } from "react-icons/ai";
-import { HiOutlineMenuAlt3, HiOutlineMenuAlt2 } from "react-icons/hi";
-import { Link } from "react-router-dom";
 import Navbar from "./Navbar";
 import "./Navigation.css";
 
@@ -15,62 +11,6 @@ const Navigation = () => {
     const body = document.body;
     body.style.overflow = "";
   };
-
-  const hamburgerIconEntry = (
-    <HiOutlineMenuAlt3
-      size="40px"
-      color="white"
-      onClick={() => setOpen(!open)}
-      onMouseEnter={() => setHovered(true)}
-      className="hamburger"
-    />
-  );
-
-  const hamburgerIconLeave = (
-    <HiOutlineMenuAlt2
-      size="60px"
-      onClick={() => setOpen(!open)}
-      onMouseLeave={() => setHovered(false)}
-      className="hamburger-leave"
-    />
-  );
-
-  const renderBurger = () => {
-    return hovered ? hamburgerIconLeave : hamburgerIconEntry;
-  };
-
-  /* const closeIcon = (
-    <AiOutlineClose
-      size="40px"
-      color="white"
-      onClick={() => unfreezeScrolling()}
-      className="close"
-    />
-  ); */
-
-  const closeIcon = (
-    <button
-      className="button-three"
-      aria-controls="primary-navigation"
-      aria-expanded="false"
-    >
-      <svg
-        stroke="var(--button-color)"
-        fill="none"
-        className="hamburger"
-        viewBox="-10 -10 120 120"
-        width="250"
-      >
-        <path
-          className="line"
-          stroke-width="10"
-          stroke-linecap="round"
-          stroke-linejoin="round"
-          d="m 20 40 h 60 a 1 1 0 0 1 0 20 h -60 a 1 1 0 0 1 0 -40 h 30 v 70"
-        ></path>
-      </svg>
-    </button>
-  );
 
   useEffect(() => {
     setHovered(true);
@@ -111,11 +51,6 @@ const Navigation = () => {
   return (
     <>
       <div className="navigation">
-        {/* <div className="symbols">
-          <Link to="/" onClick={() => close()}></Link>
-          {open ? closeIcon : renderBurger()}
-        </div>
-        {open && <Navbar isMobile={true} closeMobileMenu={closeMobileMenu} />} */}
         <button
           className="button-three"
           aria-controls="primary-navigation"
